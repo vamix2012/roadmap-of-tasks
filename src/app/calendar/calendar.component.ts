b3038849341209fd728ef78609bd2ed9decd8c5f
Cristian <vamix2012@gmail.com>
2022-02-03 02:12:23 +0100

    refractoring and refining

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { CalendarDataService } from './../services/calendar-data.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Note } from '../models/note.model';
 import { MatDialog } from '@angular/material/dialog';
 import { NoteLabel } from '../models/noteLabel.model';
@@ -11,6 +11,7 @@ import { EditTicketComponent } from '../edit-ticket/edit-ticket.component';
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
+  allNotes: Note[] = [];
   // Start of first Week in ms
   @Input() calendarWeekStart: number = 1641164400000;
   //one Week in ms
@@ -21,19 +22,27 @@ export class CalendarComponent implements OnInit {
   calendarWeekEnd: number = this.calendarWeekStart + this.oneWeek;
   //start index used for mapping each note or array of notes in the template
   @Input() startIndex = 0;
-
-  // Here are the mapped values of each section
+  cardWidth!: number;
+  ratio: number = 2.45; // Here are the mapped values of each section
   filteredFrontend: any = [];
   filteredBackend: any = [];
   filteredSecurity: any = [];
   // used for determining the offset of the stacked cards
-  stackOffset = 15;
+  stackOffsetX = 5;
+  stackOffsetY = 22;
   // values used for the filter on the left of the toolbar
   @Input() isFrontend: boolean = true;
   @Input() isBackend: boolean = true;
   @Input() isSecurity: boolean = true;
   // value used for Darkmode switch
   @Input() isDarkMode: boolean = false;
+  mappedTasks: any[] = [
+    {
+      frontend: [],
+      backend: [],
+      security: [],
+    },
+  ];
   //value used to keep track of the Calendar Week
   @Input() calendarWeek: number = 1;
   //value initialized to store labels data
@@ -45,6 +54,8 @@ export class CalendarComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.checkInnerWidth();
+    this.cardWidth = window.innerWidth / this.ratio;
     //Get request to get the labels data
     this.dataService
       .getLabels(this.dataService.noteLabelsEndpoint)
@@ -55,50 +66,81 @@ export class CalendarComponent implements OnInit {
     this.dataService
       .getAllNotes(this.dataService.allNotesEndpoint)
       .subscribe((note) => {
-        //container initialized to sort every note for each section
-        let frontend: Note[] = [];
-        let backend: Note[] = [];
-        let security: Note[] = [];
-
-        for (let ticket of note.notes) {
-          // converting date from seconds to miliseconds
+        this.allNotes = note.notes;
+        for (let ticket of this.allNotes) {
+          // converting date start and end timestamp from seconds to miliseconds
           ticket.startDate *= 1000;
           ticket.endDate *= 1000;
-
-          if (ticket.labels.includes(1)) {
-            frontend.push(ticket);
-          }
-          if (ticket.labels.includes(2)) {
-            backend.push(ticket);
-          }
-          if (ticket.labels.includes(3)) {
-            security.push(ticket);
-          }
         }
-        // Mapping function call for every section
-        this.mapTickets(frontend, this.filteredFrontend);
-        this.mapTickets(backend, this.filteredBackend);
-        this.mapTickets(security, this.filteredSecurity);
+        this.mapTickets();
       });
   }
 
-  // Mapping every note to it's coresponding  row and day
-  mapTickets(notes: any, mapedNotes: any) {
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkInnerWidth();
+  }
+
+  checkInnerWidth() {
+    if (window.innerWidth > 1682) {
+      this.ratio = 2.45;
+      this.cardWidth = window.innerWidth / this.ratio;
+    } else {
+      this.ratio = 2.01;
+      this.cardWidth = window.innerWidth / this.ratio;
+    }
+  }
+
+  // sortTasks(tasks: Note[]) {
+  //   //container initialized to sort every note for each section
+  //   for (let ticket of tasks) {
+  //
+  //
+  //     // sort every task to it's corresponding label
+  //     if (ticket.labels.includes(1)) {
+  //       frontend.push(ticket);
+  //     }
+  //     if (ticket.labels.includes(2)) {
+  //       backend.push(ticket);
+  //     }
+  //     if (ticket.labels.includes(3)) {
+  //       security.push(ticket);
+  //     }
+  //   }
+  //   // Mapping function call for every section
+  //   this.mapTickets(frontend, this.filteredFrontend);
+  //   this.mapTickets(backend, this.filteredBackend);
+  //   this.mapTickets(security, this.filteredSecurity);
+  // }
+
+  // Mapping every note to it's coresponding day
+  mapTickets() {
     let iterator = 0;
     let start = this.calendarWeekStart;
-    for (let i = 0; i < 20; i++) {
-      mapedNotes[i] = [];
-      for (let note of notes) {
+    for (let i = 0; i < this.allNotes.length; i++) {
+      this.mappedTasks[0].frontend[i] = [];
+      this.mappedTasks[0].backend[i] = [];
+      this.mappedTasks[0].security[i] = [];
+      for (let note of this.allNotes) {
         if (
           start - note.startDate > -this.oneDay &&
           start - note.startDate < this.oneDay
         ) {
-          mapedNotes[i].push(note);
+          if (note.labels.includes(1)) {
+            this.mappedTasks[0].frontend[i].push(note);
+          }
+          if (note.labels.includes(2)) {
+            this.mappedTasks[0].backend[i].push(note);
+          }
+          if (note.labels.includes(3)) {
+            this.mappedTasks[0].security[i].push(note);
+          }
         }
       }
       iterator++;
       start += this.oneDay;
     }
+    console.log(this.mappedTasks[0]);
   }
 
   // Open dialog to edit Ticket
